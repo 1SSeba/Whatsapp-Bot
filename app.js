cc63709b4ccead4cbb226dd4c5319a8c340d5505
Sebastian Cruz <165743365+1SSeba@users.noreply.github.com>
2024-08-23 09:32:45 -0400

    1

---
+++ app.js
@@ -0,0 +1,34 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+const flowPrincipal = addKeyword(['hola'])
+    .addAnswer('*Conexion Estelar*')
+    .addAnswer(
+        [
+            'Atendiendo su llamado de emergencia.'
+        ], 
+        null,
+        null,
+    )
+
+
+
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
